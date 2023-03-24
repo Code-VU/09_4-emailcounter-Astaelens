42fe411e090d8e07f59ce987bd15e8123c33e878
Alex Staelens <Alex.Staelens@vu.com>
2023-03-24 15:27:49 -0500

    used line counter from previous assignments and added max fuction to calc the key for the maximum

--- counter.py
+++ counter.py
@@ -3,7 +3,21 @@ def countEmail():
     name = input("Enter file:")
     if len(name) < 1 : name = "mbox-short.txt"
     handle = open(name)
-        
+    emails=dict()
+    for line in handle:
+        if line.startswith('From '):
+            words=line.split()
+            emails[words[1]]= emails.get(words[1],0)+1
+
+    #maxi=0    
+    #email=None
+    #for list in emails:
+    #    if emails[list]>maxi:
+    #        maxi=emails[list]
+    #        email= list
+    maxi=max(emails.keys(), key=(lambda k: emails[k]))
+
+    print(maxi, emails[maxi])
 
 ## if you want to test locally run > python counter.py
 if __name__ == "__main__":
